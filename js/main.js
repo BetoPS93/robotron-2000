f35ade5b04b119f2c7d17c30392989706a56c408
BetoPS93 <adalbertops93@live.com>
2023-01-02 19:59:29 -0300

    aula 03

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
-const elemento = document.querySelector("#calcular");
-const resultado = document.querySelector(".resultado");
+const subtrair = document.querySelector("#subtrair");
+const somar = document.querySelector("#somar");
+const braco = document.querySelector("#braco");
 
-elemento.addEventListener("click", (evento) => {
-  resultado.innerHTML = "Fui clicado"
-})
+const controle = document.querySelectorAll(".controle-ajuste");
+
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent)
+    })
+});
+
+/* somar.addEventListener("click", () => { manipulaDados("+") });
+
+subtrair.addEventListener("click", () => { manipulaDados("-") }); */
+
+function manipulaDados(operacao) {
+    if (operacao === "-") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}
